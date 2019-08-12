3c32d4eff11c55f85457c081739fff5eed50920c
patilvishalvs <patilvishalvs@gmail.com>
2019-08-12 23:20:58 +0530

    src

---
+++ src/GenericCRUDServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PatilVishalVS\GenericCRUD;
+
+use Illuminate\Support\ServiceProvider;
+
+class GenericCRUDServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
